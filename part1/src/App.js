0463b096ee93422163d2224f1191b92db1ece8ee
Dev Gustavo <113487971+Devgustavogomes@users.noreply.github.com>
2025-06-11 08:35:10 -0300

    Add files via upload

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,16 +1,55 @@
+const Header = (props) => {
+  console.log(props);
+  <>
+    <h1>{props.course}</h1>
+  </>
+}
+const Parts = (props) => {
+  console.log(props);
+  <>
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  </>
+}
+const Content = (props) => {
+  console.log(props);
+  <>
+    <Parts part={props.parts[0]} exercises={props.exercises[0]} />
+    <Parts part={props.parts[1]} exercises={props.exercises[1]} />
+    <Parts part={props.parts[2]} exercises={props.exercises[2]} />☻
+  </>
+}
+
+const Total = (props) => {
+  console.log(props);
+  <p>Number of exercises {props.total}</p>
+}
+
 const App = () => {
-  const hoje = new Date()
-  const a = 10
-  const b = 20
-  console.log(hoje, a + b)
+  const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <p>Olá, mundo! Hoje é {hoje.toString()}</p>
-      <p>
-        {a} mais {b} é {a + b}
-      </p>
-      
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={totalExercises} />
     </div>
   )
 }

4a4f5f4be613fa8246ab56af594fae48eb4fea67
Gustavo Gomes <devgustavogomes31@gmail.com>
2025-09-16 15:54:13 -0300

    bug fixed

--- part12-containers-applications-main/todo-app/todo-backend/mongo/index.js
+++ part12-containers-applications-main/todo-app/todo-backend/mongo/index.js
@@ -1,10 +1,28 @@
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+// Coloque estas duas linhas no topo de tudo!
+require("dotenv").config();
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(
+  "URL de Conexão que o Node está tentando usar:",
+  process.env.MONGO_URL
+);
+const mongoose = require("mongoose");
 
+// ... outras importações como 'express', etc.
 
-module.exports = {
-  Todo
-}
+const MONGO_URL = process.env.MONGO_URL;
+
+// Adicione um log para ter certeza de que a URL está sendo carregada
+console.log("Tentando conectar com a URL:", MONGO_URL);
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    // A conexão funcionou!
+    console.log("✅ Conectado ao MongoDB com sucesso!");
+  })
+  .catch((error) => {
+    // A conexão falhou!
+    console.error("❌ Erro ao conectar ao MongoDB:", error.message);
+  });
+
+// ... resto do seu código do servidor (app.listen, etc.)
